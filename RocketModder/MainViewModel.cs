8738cd9b01eea060e58d18cfaf19d8bbb93d8925
MikaelStalvik <mikael.stalvik@gmail.com>
2020-05-23 18:56:26 +0200

    Initial commit

---
+++ RocketModder/MainViewModel.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using Microsoft.Win32;
+using RocketModder.Annotations;
+
+namespace RocketModder
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<RocketFile> SelectedRocketFiles = new ObservableCollection<RocketFile>();
+
+        private RocketFile _selectedItem;
+        public RocketFile SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand AddFilesCommand { get; set; }
+        public RelayCommand SaveFileCommand { get; set; }
+
+        public MainViewModel()
+        {
+            AddFilesCommand = new RelayCommand(o =>
+            {
+                var ofd = new OpenFileDialog {Multiselect = true, Filter = "Rocket files|*.rocket"};
+                if (ofd.ShowDialog() == true)
+                {
+                    foreach (var file in ofd.FileNames)
+                    {
+                        SelectedRocketFiles.Add(new RocketFile {Filename = file, Offset = 0});
+                    }
+                    OnPropertyChanged(nameof(SelectedRocketFiles));
+                }
+            });
+            SaveFileCommand = new RelayCommand(o =>
+            {
+                var sfd = new SaveFileDialog {Filter = "Rocket files|*.rocket"};
+                if (sfd.ShowDialog() == true)
+                {
+
+                }
+            });
+        }
+
+        private List<TrackItem> readRocketFile(string filename)
+        {
+            var xml = XDocument.Load(filename);
+            var qry = from c in xml.Descendants("track")
+                select new TrackItem
+                {
+                    Name = c.Attribute("name").Value,
+                    Folded = int.Parse(c.Attribute("folded").Value),
+                    MuteKeyCount = c.Attribute("muteKeyCount").Value,
+                    Color = c.Attribute("color").Value,
+                    Keys = from k in c.Descendants("key")
+                        select new KeyItem
+                        {
+                            Row = int.Parse(k.Attribute("row").Value),
+                            Value = double.Parse(k.Attribute("value").Value, CultureInfo.InvariantCulture),
+                            Interpolation = int.Parse(k.Attribute("interpolation").Value)
+                        }
+                };
+            return qry.ToList();
+        }
+
+        private string generateFile(List<List<TrackItem>> rockets, List<int> offsets)
+        {
+            var resdata = new List<TrackItem>();
+            for (var i = 0; i < rockets.Count; i++)
+            {
+                var rocket = rockets[i];
+                for (var j = 0; j < rocket.Count; j++)
+                {
+                    var item = rocket[j];
+                    var existing = resdata.FirstOrDefault(x => x.Name.Equals(item.Name));
+                    if (existing == null)
+                    {
+                        resdata.Add(item);
+                    }
+                    else
+                    {
+                        // add all keys
+                        var ofs = offsets[i];
+                        var existingKeys = existing.Keys.ToList();
+                        foreach (var key in item.Keys)
+                        {
+                            existingKeys.Add(new KeyItem
+                            {
+                                Row = key.Row + ofs,
+                                Value = key.Value,
+                                Interpolation = key.Interpolation
+                            });
+                        }
+
+                        existing.Keys = existingKeys;
+                    }
+                }
+            }
+
+            // all data aggregated, generate XML
+            var xd = new XDocument(new XElement("rootElement",
+                new XElement("tracks",
+                    new XAttribute("rows", 10000),
+                    new XAttribute("startRow", 0),
+                    new XAttribute("endRow", 10000),
+                    new XAttribute("rowsPerBeat", 8),
+                    new XAttribute("beatsPerMin", 134),
+                    from track in resdata
+                    select new XElement("track",
+                        new XAttribute("name", track.Name),
+                        new XAttribute("color", track.Color),
+                        new XAttribute("folded", track.Folded),
+                        from key in track.Keys
+                        select new XElement("key",
+                            new XAttribute("row", key.Row),
+                            new XAttribute("value", key.Value),
+                            new XAttribute("interpolation", key.Interpolation)
+                        )
+                    )
+                )));
+
+
+            return string.Empty;
+        }
+    }
+}
---
+++ RocketModder/Models.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketModder
+{
+    public class TrackStruct
+    {
+        public string Name { get; set; }
+        public bool Folded { get; set; }
+        public string MuteKeyCount { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class TrackItem
+    {
+        public string Name { get; set; }
+        public int Folded { get; set; }
+        public string MuteKeyCount { get; set; }
+        public string Color { get; set; }
+        public IEnumerable<KeyItem> Keys { get; set; }
+    }
+
+    public class KeyItem
+    {
+        public int Row { get; set; }
+        public double Value { get; set; }
+        public int Interpolation { get; set; }
+    }
+
+    public class RocketFile
+    {
+        public string Filename { get; set; }
+        public int Offset { get; set; }
+    }
+
+}
